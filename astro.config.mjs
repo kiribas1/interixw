6270c8429171caede0f8515375c0ce2213be569f
Kiribas1 <kiribati.islands@proton.me>
2026-06-29 14:17:19 +0000

    Rename to interixw.link, remove registration, update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://interixw.com',
+  site: 'https://interixw.link',
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
